2bd20447a61202a0cfc03f52fbe9d650843c72f0
fpem123 <ghtjq3909@naver.com>
2020-12-12 17:10:29 +0900

    add inc function and test code for it

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,11 +1,21 @@
 
 public class Calc {
+	private int r = 0;
+
 	public int add(int x, int y) { // 2차 커밋을 위한 내용변경
 		return x + y;
 	}
 
 	public int sub(int x, int y) {
 		return x - y;
+	}
+
+	public int inc(int d){
+		r += d;
+	}
+
+	public int getResult() {
+		return r;
 	}
 }
 
