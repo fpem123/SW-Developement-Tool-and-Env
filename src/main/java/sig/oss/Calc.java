b43bb4eb1da19ad1b1b66e02a915ea47a256db55
Lee Hoseop <ghtjq3909@naver.com>
2020-12-12 17:01:52 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,11 +1,17 @@
 
 public class Calc {
+	public int r = 0;
+	
 	public int add(int x, int y) { // 2차 커밋을 위한 내용변경
 		return x + y;
 	}
 
 	public int sub(int x, int y) {
 		return x - y;
+	}
+	
+	public void dec(int d){	//
+		r -= d;
 	}
 }
 
